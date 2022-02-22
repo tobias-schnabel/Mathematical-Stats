5a3854a051f9030dac9e675b9e4c08db3983d47c
chumasha <100204424+chumasha@users.noreply.github.com>
2022-02-22 12:42:59 +0100

    ok

--- TeamProject.R
+++ TeamProject.R
@@ -2,27 +2,29 @@
 library("zoo", "ggplot2", "tidy")
 
 ##import data
+#########
+#/Users/chumasharajapakshe/Documents/GitHub/Mathematical_Stats/Data/AnnualTemp.csv
 
 #annual
-annualData <- read.csv("~/R projects/Mathematical_Stats/Data/AnnualTemp.csv", 
+annualData <- read.csv("/Users/chumasharajapakshe/Documents/GitHub/Mathematical_Stats/Data/AnnualTemp.csv", 
                  header = TRUE, sep = ";", quote = "\"'", dec = ",", 
                  fill = TRUE, comment.char="")
 colnames(annualData) <- c("Year", "Eelde", "De Bilt", "Maastricht") 
 #monthly
-monthlyData <- read.csv("~/R projects/Mathematical_Stats/Data/MonthlyTemp.csv", 
+monthlyData <- read.csv("/Users/chumasharajapakshe/Documents/GitHub/Mathematical_Stats/Data/MonthlyTemp.csv", 
                         header = TRUE, sep = ";", quote = "\"'", dec = ",", 
                         fill = TRUE, comment.char="")
 #label
 colnames(monthlyData) <- c("Year", "Eelde", "De Bilt", "Maastricht") 
 #daily
-dailyData <- read.csv("/Users/ts/R projects/Mathematical_Stats/Data/DailyTemp.csv", 
+dailyData <- read.csv("/Users/chumasharajapakshe/Documents/GitHub/Mathematical_Stats/Data/DailyTemp.csv", 
                         header = TRUE, sep = ";", quote = "\"'", dec = ",", 
                         fill = TRUE, comment.char="")
 #label
 colnames(dailyData) <- c("Year", "Eelde", "De Bilt", "Maastricht") 
 
 #smoothed
-monthlySmoothedData <- read.csv("/Users/ts/R projects/Mathematical_Stats/Data/SmoothedMonthlyTemp.csv", 
+monthlySmoothedData <- read.csv("//Users/chumasharajapakshe/Documents/GitHub/Mathematical_Stats/Data/SMTemp.csv", 
                                 header = TRUE, sep = ";", quote = "\"'", dec = ",", 
                                 fill = TRUE, comment.char="")
 #label
