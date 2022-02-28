6c20e931b4aaed6224b061fd5721d42ea008129e
ts <ctobiasschnabel@gmail.com>
2022-02-28 21:47:00 +0100

    updated file paths, they should now change automatically depending on who executes the code

--- TeamProject.R
+++ TeamProject.R
@@ -1,30 +1,34 @@
 #load packages
-library("zoo", "ggplot2", "tidy")
+library("zoo", "ggplot2", "tidyverse", "data.table")
 
 ##import data
 #########
 #/Users/chumasharajapakshe/Documents/GitHub/Mathematical_Stats/Data/AnnualTemp.csv
+#/Users/ts/Git/Mathematical_Stats
+Paths = c("/Users/ts/Git/Mathematical_Stats", "Users/chumasharajapakshe/Documents/GitHub/Mathematical_Stats")
+names(Paths) = c("ts", "chumasharajapakshe")
+setwd(Paths[Sys.info()[7]])
 
 #annual
-annualData <- read.csv("/Users/chumasharajapakshe/Documents/GitHub/Mathematical_Stats/Data/AnnualTemp.csv", 
+annualData <- read.csv("./Data/AnnualTemp.csv", 
                  header = TRUE, sep = ";", quote = "\"'", dec = ",", 
                  fill = TRUE, comment.char="")
 colnames(annualData) <- c("Year", "Eelde", "De Bilt", "Maastricht") 
 #monthly
-monthlyData <- read.csv("/Users/chumasharajapakshe/Documents/GitHub/Mathematical_Stats/Data/MonthlyTemp.csv", 
+monthlyData <- read.csv("./Data/MonthlyTemp.csv", 
                         header = TRUE, sep = ";", quote = "\"'", dec = ",", 
                         fill = TRUE, comment.char="")
 #label
 colnames(monthlyData) <- c("Year", "Eelde", "De Bilt", "Maastricht") 
 #daily
-dailyData <- read.csv("/Users/chumasharajapakshe/Documents/GitHub/Mathematical_Stats/Data/DailyTemp.csv", 
+dailyData <- read.csv("./Data/DailyTemp.csv", 
                         header = TRUE, sep = ";", quote = "\"'", dec = ",", 
                         fill = TRUE, comment.char="")
 #label
 colnames(dailyData) <- c("Year", "Eelde", "De Bilt", "Maastricht") 
 
 #smoothed
-monthlySmoothedData <- read.csv("//Users/chumasharajapakshe/Documents/GitHub/Mathematical_Stats/Data/SMTemp.csv", 
+monthlySmoothedData <- read.csv("./Data/SMTemp.csv", 
                                 header = TRUE, sep = ";", quote = "\"'", dec = ",", 
                                 fill = TRUE, comment.char="")
 #label
@@ -42,4 +46,4 @@ subsample_1 <- subset(annualData, Year >= 1907 & Year < 1958)
 zoo::rollapply(annualData$Eelde, 10, mean)
 zoo::rollmean(annualData$'De Bilt', 10)
 
-#hgjhvh
+
