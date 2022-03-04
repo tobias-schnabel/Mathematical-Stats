9c66c918e68c7db785e807d03429a83a6246c40a
ts <ctobiasschnabel@gmail.com>
2022-03-04 18:43:22 +0100

    1. cleaned up package and data import 2. managed to subset ten year means (finally) 3. built function to subset means on custom window

--- TeamProject.R
+++ TeamProject.R
@@ -2,7 +2,7 @@
 
 rm(list = ls(all = TRUE)) ###CLEAR ALL
 # Package names
-packages <- c("data.table", "dplyr", "zoo", "tidyr", "ggplot2", "tidyverse", "xtable", "knitr")
+packages <- c("data.table", "dplyr", "zoo", "tidyr", "ggplot2", "tidyverse", "xtable", "knitr", "stargazer")
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
@@ -22,27 +22,40 @@ setwd(Paths[Sys.info()[7]])
 #########################DATA IMPORT#########################
 
 #annual
-da <- fread('./Data/AnnualTemp.csv')
+da <- fread('./Data/AnnualTemp.csv', dec=",")
 colnames(da) <- c('year', 'de_bilt', 'eelde', 'maastricht')
 #monthly
-dm <- fread('./Data/MonthlyTemp.csv')
+dm <- fread('./Data/MonthlyTemp.csv', dec=",")
 colnames(dm) <- c('month', 'de_bilt', 'eelde', 'maastricht')
 
 #smoothed
-dms <- fread('./Data/SMTemp.csv')
+dms <- fread('./Data/SMTemp.csv', dec=",")
 colnames(dms) <- c('month', 'de_bilt', 'eelde', 'maastricht')
 
 #daily
-dd <- fread('./Data/DailyTemp.csv')
+dd <- fread('./Data/DailyTemp.csv', dec=",")
 colnames(dd) <- c('date', 'de_bilt', 'eelde', 'maastricht')
 ########################DATA ANALYSIS########################
 
-#Split the sample in a number of subsamples, and 
-#compare average temperatures across the subsamples.
-#non-overlapping subsamples
+#original code then turned into function
+# rollmeans <- frollmean(da[, .(de_bilt, eelde, maastricht)], 10)
+# setDT(rollmeans)
+# keepYears <- seq(from = 10, to = 120, by = 10)
+# rollmeans[, ID := .I ] #data starts in 1907 (first year from which onwards we can computer a decade, ID starts at 1)
+# meantable <- rollmeans[ID %%10 == 0 , .(Year = ID + 1897, Eelde = V1, 'De Bilt' = V2, Maastricht = V3)]
 
-#tenyearmeans <- da[, .(meanMST = mean(maastricht) )]
+xYearAvg <- function(x){
+  #generate data.table with rolling means, window=x
+  xYrm <- as.data.table(frollmean(da[, .(de_bilt, eelde, maastricht)], x))[, ID := .I ]
+  keepYearsX <- seq(from = 1, to = 120, by = x) #used to drop all other rows
+  
+  #data starts in 1907, first 10 years of rollmean are NA, years in this table indicate start of window
+  xYmt <- xYrm[ID %%x == 0 , .(Year = ID + 1897, Eelde = V1, 'De Bilt' = V2, Maastricht = V3)]
+  return(xYmt)
+}
 
-
-
+meanTable10 <- xYearAvg(10)
+view(meanTable10)
+meanTable5 <- xYearAvg(5)
+view(meanTable5)
 
