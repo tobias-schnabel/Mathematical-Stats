935f0b6a69e60917d43f5e572de45d21c7e2f0bc
ts <ctobiasschnabel@gmail.com>
2022-04-02 18:09:01 +0200

    added 2 packages

--- Tables.R
+++ Tables.R
@@ -4,9 +4,13 @@ stargazer(daSS, out.header = F, title = "Annual Data",
           out = "/Users/ts/Dropbox/Apps/Overleaf/Project Mathematical Statistics/Tables/AS" )
 
 
+dmSS <- dmC[,.("De Bilt" = De.Bilt, Eelde, Maastricht)]
+stargazer(dmSS, out.header = F, title = "Smoothed Monthly Data",
+          out = "/Users/ts/Dropbox/Apps/Overleaf/Project Mathematical Statistics/Tables/MS" )
+
 dmsSS <- dmsC[,.("De Bilt" = De.Bilt, Eelde, Maastricht)]
 stargazer(dmsSS, out.header = F, title = "Smoothed Monthly Data",
-          out = "/Users/ts/Dropbox/Apps/Overleaf/Project Mathematical Statistics/Tables/MS" )
+          out = "/Users/ts/Dropbox/Apps/Overleaf/Project Mathematical Statistics/Tables/MSS" )
 
 ddSS <- ddC[,.("De Bilt" = De.Bilt, Eelde, Maastricht)]
 stargazer(ddSS, out.header = F, title = "Daily Data",
--- TeamProject.R
+++ TeamProject.R
@@ -3,7 +3,7 @@
 rm(list = ls(all = TRUE)) ###CLEAR ALL
 # Package names
 packages <- c("data.table", "dplyr", "zoo", "tidyr", "ggplot2", "ggthemes", "scales",
-              "tidyverse", "xtable", "knitr", "stargazer", "patchwork", "remotes")
+              "tidyverse", "xtable", "knitr", "stargazer", "patchwork", "remotes", "broom", "purrr")
 # package grateful must be installed by hand# install.packages("remotes")
 remotes::install_github("Pakillo/grateful")
 # Install packages not yet installed
@@ -36,6 +36,8 @@ colnames(dms) <- c('month', 'de_bilt', 'eelde', 'maastricht')
 #daily
 dd <- fread('./Data/DailyTemp.csv', dec=",")
 colnames(dd) <- c('date', 'de_bilt', 'eelde', 'maastricht')
+
+
 ########################DATA ANALYSIS########################
 # the following functions all work the same way: 
 # 1. They first subset to a new data.table the annual/monthly/monthly (smoothed)/daily 
@@ -46,90 +48,92 @@ colnames(dd) <- c('date', 'de_bilt', 'eelde', 'maastricht')
 # 3. They then drop all rows outside the specified time window so that we are left with 
 # a subset of data with e.g. 10 year means or 6 day medians
 # We use these to experiment with different subsets 
-
-xYearStat <- function(x, STATNAME){
-  #generate data.table with rolling means, window=x
-  xYrS <- as.data.table(frollapply(da[, .(de_bilt, eelde, maastricht)], x, FUN = STATNAME))[, ID := .I ]
-  dateSet <- da[, ID := .I][, .(year, ID)]
-  xYrStatInclYr <- cbind(xYrS, dateSet)
-  xYST <- xYrStatInclYr[ID %% x == 0 , .(Year = year, Eelde = V1, 'De Bilt' = V2, Maastricht = V3)]
-  return(xYST)
-}
-
-xYearYoverlapStat <- function(x, y, STATNAME){
-  #generate data.table with rolling means, window=x
-  xYrS <- as.data.table(frollapply(da[, .(de_bilt, eelde, maastricht)], x, FUN = STATNAME))[, ID := .I ]
-  dateSet <- da[, ID := .I][, .(year, ID)]
-  xYrStatInclYr <- cbind(xYrS, dateSet)
-  z = x - y
-  xYST <- xYrStatInclYr[ID %% z == 0 , .(Year = year, Eelde = V1, 'De.Bilt' = V2, Maastricht = V3)]
-  return(xYST)
-}
-
-xMonthStat <- function(x, STATNAME){
+#functions
+{
+  xYearStat <- function(x, STATNAME){
+    #generate data.table with rolling means, window=x
+    xYrS <- as.data.table(frollapply(da[, .(de_bilt, eelde, maastricht)], x, FUN = STATNAME))[, ID := .I ]
+    dateSet <- da[, ID := .I][, .(year, ID)]
+    xYrStatInclYr <- cbind(xYrS, dateSet)
+    xYST <- xYrStatInclYr[ID %% x == 0 , .(Year = year, Eelde = V1, 'De Bilt' = V2, Maastricht = V3)]
+    return(xYST)
+  }
+  
+  xYearYoverlapStat <- function(x, y, STATNAME){
+    #generate data.table with rolling means, window=x
+    xYrS <- as.data.table(frollapply(da[, .(de_bilt, eelde, maastricht)], x, FUN = STATNAME))[, ID := .I ]
+    dateSet <- da[, ID := .I][, .(year, ID)]
+    xYrStatInclYr <- cbind(xYrS, dateSet)
+    z = x - y
+    xYST <- xYrStatInclYr[ID %% z == 0 , .(Year = year, Eelde = V1, 'De.Bilt' = V2, Maastricht = V3)]
+    return(xYST)
+  }
+  
+  xMonthStat <- function(x, STATNAME){
     xMosRS <- as.data.table(frollapply(dm[, .(de_bilt, eelde, maastricht)], x, FUN = STATNAME))[, ID := .I ]
     dateSet <- dm[, ID := .I][, .(month, ID)]
     xMosRSInclMonth <- cbind(xMosRS, dateSet)
     xMosStat <- xMosRSInclMonth[(ID) %% x == 0 , .(Month = month , Eelde = V1, 'De Bilt' = V2, Maastricht = V3)]
     return(xMosStat)
-}
-
-xMonthSmoothedStat <- function(x, STATNAME){
-  xMosSRS <- as.data.table(frollapply(dms[, .(de_bilt, eelde, maastricht)], x, FUN = STATNAME))[, ID := .I ]
-  dateSet <- dms[, ID := .I][, .(month, ID)]
-  xMosSRSInclMonth <- cbind(xMosSRS, dateSet)
-   xMosSStat <- xMosSRSInclMonth[(ID) %% x == 0 , .(Month = month , Eelde = V1, 'De Bilt' = V2, Maastricht = V3)]
-  return(xMosSStat)
-}
-
-xDayStat <- function(x, STATNAME){
-  xDayRS <- as.data.table(frollapply(dd[, .(de_bilt, eelde, maastricht)], x, FUN = STATNAME))[, ID := .I ]
-  dateSet <- dd[, ID := .I][, .(date, ID)]
-  xDayRSInclDate <- cbind(xDayRS, dateSet)
-  xMosSStat <- xDayRSInclDate[(ID) %% x == 0 , .(Date = date, Eelde = V1, 'De Bilt' = V2, Maastricht = V3)]
-  return(xMosSStat)
-}
-
-subsetMonth <- function(mm){
-  mm <- str_pad(as.character(mm), 2, side = "left", pad = '0')
+  }
   
-  subset <- dd[, DateCol := as.Date(as.character(date), format = "%Y%m%d")
-              ][, month := format(as.Date(DateCol), "%m")]
-  compSet <- subset[month == mm, .(DateCol, eelde, de_bilt, maastricht)]
-return(compSet)
-}
-
-subsetMonthLong <- function(mm){
-  mm <- str_pad(as.character(mm), 2, side = "left", pad = '0')
+  xMonthSmoothedStat <- function(x, STATNAME){
+    xMosSRS <- as.data.table(frollapply(dms[, .(de_bilt, eelde, maastricht)], x, FUN = STATNAME))[, ID := .I ]
+    dateSet <- dms[, ID := .I][, .(month, ID)]
+    xMosSRSInclMonth <- cbind(xMosSRS, dateSet)
+    xMosSStat <- xMosSRSInclMonth[(ID) %% x == 0 , .(Month = month , Eelde = V1, 'De Bilt' = V2, Maastricht = V3)]
+    return(xMosSStat)
+  }
   
-  subset <- dd[, DateCol := as.Date(as.character(date), format = "%Y%m%d")
-  ][, month := format(as.Date(DateCol), "%m")]
-  compSet <- subset[month == mm, .(Month = month, Eelde = eelde, De.Bilt = de_bilt, Maastricht = maastricht)]
-  ret <- melt(compSet, id.vars = "Month", measure.vars = c("Maastricht", "Eelde", "De.Bilt"),
-              variable.factor = T, variable.name = "City", value.name = "Temperature")[, Citymean := mean(Temperature), by = City]
-  return(ret)
-}
-
-subsetDate <- function(mmdd){
-  mmdd <- str_pad(as.character(mmdd), 4, side = "left", pad = '0')
+  xDayStat <- function(x, STATNAME){
+    xDayRS <- as.data.table(frollapply(dd[, .(de_bilt, eelde, maastricht)], x, FUN = STATNAME))[, ID := .I ]
+    dateSet <- dd[, ID := .I][, .(date, ID)]
+    xDayRSInclDate <- cbind(xDayRS, dateSet)
+    xMosSStat <- xDayRSInclDate[(ID) %% x == 0 , .(Date = date, Eelde = V1, 'De Bilt' = V2, Maastricht = V3)]
+    return(xMosSStat)
+  }
   
-  subset <- dd[, DateCol := as.Date(as.character(date), format = "%Y%m%d")
-  ][, md := format(as.Date(DateCol), "%m%d")]
+  subsetMonth <- function(mm){
+    mm <- str_pad(as.character(mm), 2, side = "left", pad = '0')
+    
+    subset <- dd[, DateCol := as.Date(as.character(date), format = "%Y%m%d")
+    ][, month := format(as.Date(DateCol), "%m")]
+    compSet <- subset[month == mm, .(DateCol, eelde, de_bilt, maastricht)]
+    return(compSet)
+  }
   
-  compSet <- subset[mmdd == md, .(Date = md, eelde, de_bilt, maastricht)]
-  return(compSet)
-}
-
-subsetDateLong <- function(mmdd){
-  mmdd <- str_pad(as.character(mmdd), 4, side = "left", pad = '0')
+  subsetMonthLong <- function(mm){
+    mm <- str_pad(as.character(mm), 2, side = "left", pad = '0')
+    
+    subset <- dd[, DateCol := as.Date(as.character(date), format = "%Y%m%d")
+    ][, month := format(as.Date(DateCol), "%m")]
+    compSet <- subset[month == mm, .(Month = month, Eelde = eelde, De.Bilt = de_bilt, Maastricht = maastricht)]
+    ret <- melt(compSet, id.vars = "Month", measure.vars = c("Maastricht", "Eelde", "De.Bilt"),
+                variable.factor = T, variable.name = "City", value.name = "Temperature")[, Citymean := mean(Temperature), by = City]
+    return(ret)
+  }
   
-  subset <- dd[, DateCol := as.Date(as.character(date), format = "%Y%m%d")
-  ][, md := format(as.Date(DateCol), "%m%d")]
+  subsetDate <- function(mmdd){
+    mmdd <- str_pad(as.character(mmdd), 4, side = "left", pad = '0')
+    
+    subset <- dd[, DateCol := as.Date(as.character(date), format = "%Y%m%d")
+    ][, md := format(as.Date(DateCol), "%m%d")]
+    
+    compSet <- subset[mmdd == md, .(Date = md, eelde, de_bilt, maastricht)]
+    return(compSet)
+  }
   
-  compSet <- subset[mmdd == md, .(Date = md, Eelde = eelde, De.Bilt = de_bilt, Maastricht = maastricht)]
-  ret <- melt(compSet, id.vars = "Date", measure.vars = c("Maastricht", "Eelde", "De.Bilt"),
-              variable.factor = T, variable.name = "City", value.name = "Temperature")[, Citymean := mean(Temperature)]
-  return(ret)
+  subsetDateLong <- function(mmdd){
+    mmdd <- str_pad(as.character(mmdd), 4, side = "left", pad = '0')
+    
+    subset <- dd[, DateCol := as.Date(as.character(date), format = "%Y%m%d")
+    ][, md := format(as.Date(DateCol), "%m%d")]
+    
+    compSet <- subset[mmdd == md, .(Date = md, Eelde = eelde, De.Bilt = de_bilt, Maastricht = maastricht)]
+    ret <- melt(compSet, id.vars = "Date", measure.vars = c("Maastricht", "Eelde", "De.Bilt"),
+                variable.factor = T, variable.name = "City", value.name = "Temperature")[, Citymean := mean(Temperature)]
+    return(ret)
+  } 
 }
 
 #gen datasets needed
@@ -185,9 +189,9 @@ if (Sys.info()[7] == "ts") {
                 bib.file = "grateful.bib")
 
 ########################Do Plots & Tables################################
-source("Tidy.R")
-source("Plots.R")
-source("Tables.R")
+invisible(source("Tidy.R")) 
+invisible(source("Plots.R"))
+invisible(source("Tables.R"))
 ########################R File########################
 file.copy('TeamProject.R', '/Users/ts/Dropbox/Apps/Overleaf/Project Mathematical Statistics/', overwrite = T)
 }
